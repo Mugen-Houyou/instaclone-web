c8f3d969dd653657ef12def30f8a6711f348137c
Mugen-Houyou <a4r2@daum.net>
2022-09-28 21:51:12 -0400

    2022-09-28, hashtags and more...

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import sanitizeHtml from "sanitize-html";
+import { parseValue } from "graphql";
+import { Link } from "react-router-dom";
+import React from "react";
 
 import { FatText } from "../shared/shared";
 
@@ -9,14 +13,34 @@ const CommentContainer = styled.div`
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark{
+    background-color: inherit;
+    color: ${(props) => props.theme.accent };
+    cursor:pointer;
+    text-decoration: underline;
+  }
 `;
 
 
 const Comment = ({author, payload}) =>{
-
+  // const sanitizedPayload = sanitizeHtml(payload.replace(/#[\w]+/g, "<mark>$&</mark>"),{
+  //   allowedTags:["mark"]
+  // });
   return <CommentContainer>
     <FatText>{author}</FatText>
-    <CommentCaption>{payload}</CommentCaption>
+    {/* <CommentCaption dangerouslySetInnerHTML={{
+      __html:sanitizedPayload
+    }}   /> */}
+    <CommentCaption>{payload.split(" ").map((word,index,array) => 
+    <React.Fragment key={index}>
+      {/#[\w]+/.test(word) ? 
+        <Link to={`/hashtags/${word}`}>{word}</Link>
+      : word }
+        
+      {index !== array.length && ' '}
+
+    </React.Fragment>
+    )}</CommentCaption>    
   </CommentContainer>
     
 }
